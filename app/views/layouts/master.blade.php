c3b0fbcea1633e1032e245f93a990c919f5f66fa
nicolette <nicolette.williams8@icloud.com>
2016-06-08 18:12:52 -0500

    added materialize dropbutton and delete bootstrap cdns

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,16 +7,13 @@
     <link href='https://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+
+
 
 	{{ HTML::style('/css/custom_blog.css') }}
 </head>
--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,3 +1,4 @@
+       
 <nav>
    	<div class="nav-wrapper grey darken-3">
       	<span class="padding"><i class="fa fa-hand-lizard-o brand-logo lizard" aria-hidden="true"></i>
@@ -6,21 +7,17 @@
 
 
       	@if(Auth::check())
-		<!-- Single button -->
-		<div class="btn-group">
-		  	<button type="button" class="btn dropdown-toggle grey darken-3 navbar-links dropdown-btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		    	Hello, {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->firstname }}} <span class="caret"></span>
-		  	</button>
-		  	<ul class="dropdown-menu">
-		    	<li><a href="{{{ action('PostsController@create') }}}">Write a Post!</a></li>
-		    	<li><a href="#">All of Your Posts</a></li>
-		    	<li><a href="{{ URL::to('logout') }}">Logout</a></li>
-		  	</ul>
-		</div>
-      	@else
-      	<li>{{ HTML::link('/login', 'Log In', ['class' => 'navbar-links']) }}</li>   
-      	<li>{{ HTML::link('/signup', 'Sign Up', ['class' => 'navbar-links signup']) }}</li>   
-      	@endif
+            <a class='dropdown-button btn grey darken-3' href='#' data-activates='dropdown1'>Hello, {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->firstname }}} </a>
+            <ul id='dropdown1' class='dropdown-content'>
+                <li><a href="{{{ action('PostsController@create') }}}">Write a Post!</a></li>
+                <li><a href="#">All of Your Posts</a></li>
+                <li class="divider"></li>
+                <li><a href="{{ URL::to('logout') }}">Logout</a></li>
+            </ul>
+        @else
+            <li>{{ HTML::link('/login', 'Log In', ['class' => 'navbar-links']) }}</li>   
+            <li>{{ HTML::link('/signup', 'Sign Up', ['class' => 'navbar-links signup']) }}</li>   
+        @endif
 
 
     </ul>
@@ -31,9 +28,16 @@
 
 
 <script>
-    $(document).ready(function() {
-    	$(".dropdown-toggle").dropdown();
-	});
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
 </script>
 
 
