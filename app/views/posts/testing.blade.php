258e144a5c9917f7fc2e0142dcf7a78e27a78022
nicolette <nicolette.williams8@icloud.com>
2016-08-07 21:44:14 -0500

    j

--- app/views/posts/testing.blade.php
+++ app/views/posts/testing.blade.php
@@ -1,25 +1,25 @@
 <form name="contactform" method="post" action="send_form_email.php">
  
   <label for="first_name">First Name *</label>
-  <input  type="text" name="first_name" maxlength="50" size="30">
+  <input  type="text" name="first_name" maxlength="50" size="30" class="form-control">
  
  
  
   <label for="last_name">Last Name *</label>
-  <input  type="text" name="last_name" maxlength="50" size="30">
+  <input  type="text" name="last_name" maxlength="50" size="30" class="form-control">
  
   <label for="email">Email Address *</label>
-  <input  type="text" name="email" maxlength="80" size="30">
+  <input  type="text" name="email" maxlength="80" size="30" class="form-control">
  
  
  
   <label for="telephone">Telephone Number</label>
-  <input  type="text" name="telephone" maxlength="30" size="30">
+  <input  type="text" name="telephone" maxlength="30" size="30" class="form-control">
  
  
  
   <label for="comments">Comments *</label>
-  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+  <textarea  name="comments" maxlength="1000" cols="25" rows="6" class="form-control"></textarea>
 
   <input type="submit" value="Submit">   <a href="http://nicolettewilliams.com/email_form.php">Email Form</a>
  
