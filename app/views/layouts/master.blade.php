68ab0f6b91d328bc88f7b2b407b930a6c658465f
nicolette <nicolette.williams8@icloud.com>
2016-09-25 15:25:22 -0500

    test

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -15,7 +15,6 @@
     <link rel="stylesheet" type="text/css"  href="../css/style.css">
     <link rel="stylesheet" type="text/css" href="../css/prettyPhoto.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
-    <script type="text/javascript" src="../js/modernizr.custom.js"></script>
 </head>
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
     <div id="preloader">
